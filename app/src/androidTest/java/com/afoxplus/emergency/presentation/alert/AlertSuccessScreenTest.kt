88b14d8d542b64e2bba48c9400a6007f01bfc463
Valentin Mendoza <jmendozat13@gmail.com>
2026-09-08 16:36:33 -0500

    Fix tests

--- app/src/androidTest/java/com/afoxplus/emergency/presentation/alert/AlertSuccessScreenTest.kt
+++ app/src/androidTest/java/com/afoxplus/emergency/presentation/alert/AlertSuccessScreenTest.kt
@@ -3,7 +3,6 @@ package com.afoxplus.emergency.presentation.alert
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import androidx.compose.ui.test.onNodeWithText
-import androidx.compose.ui.test.assertDoesNotExist
 import com.afoxplus.emergency.presentation.features.alert.AlertSuccessScreen
 import com.afoxplus.emergency.presentation.ui.theme.AppemergencyTheme
 import org.junit.Rule
